8483bc94f34fc77161c51897218c2e2005f79b9f
itenpn <iten.pn@gmail.com>
2018-12-02 10:27:18 -0500

    Documentation

--- Day1/SumCode.java
+++ Day1/SumCode.java
@@ -4,14 +4,14 @@ public class SumCode{
 	
 	public static void main(String[] args) throws IOException{
 		Scanner r = new Scanner(new File("data.txt"));
-		int sum = 0;
-		ArrayList<String> values = new ArrayList<String>();
+		int sum = 0; //Take the total sum of the list
+		ArrayList<String> values = new ArrayList<String>();//How I can store the values
 		while(r.hasNext()){
-			String line = r.nextLine();
+			String line = r.nextLine(); //Get the line
 			values.add(line);
-			int num = Integer.parseInt(line.substring(1));
-			String val = line.substring(0,1);
-			if (val.equals("+"))
+			int num = Integer.parseInt(line.substring(1)); //Get an integer
+			String val = line.substring(0,1); //Figure out the sign
+			if (val.equals("+")) //Change sum respectively due to sign
 				sum += num;
 			else
 				sum -= num;
@@ -22,17 +22,17 @@ public class SumCode{
 	}
 
 	public static int DoubleFrequency(ArrayList<String> values){
-		ArrayList<Integer> sums = new ArrayList<Integer>();
+		ArrayList<Integer> sums = new ArrayList<Integer>(); //Storing previous sum values
 		int sum = 0;
-		while (true){
-		for (int i = 0; i < values.size(); i++){
-			int num = Integer.parseInt(values.get(i).substring(1));
-			String val = values.get(i).substring(0,1);
-			if (val.equals("+"))
+		while (true){ //List repeats at the end
+		for (int i = 0; i < values.size(); i++){ //Loops through list
+			int num = Integer.parseInt(values.get(i).substring(1)); //Parsing integer
+			String val = values.get(i).substring(0,1); //finding sign
+			if (val.equals("+")) //change sum respectively
 				sum += num;
 			else
 				sum -= num;
-			for (int z = 0; z < sums.size(); z++){
+			for (int z = 0; z < sums.size(); z++){ //find if sum is same as previous sum
 				if (sum == sums.get(z))
 					return sum;						
 			}	
--- Day2/ID.hs
+++ Day2/ID.hs
@@ -3,35 +3,47 @@ import qualified Data.List as L
 import System.IO
 
 main = do
+  --open the file
   contents <- readFile "data.txt"
+  --parse file into a list of characters and times they occur
   let x = Prelude.map frequency $ L.lines contents
+  --create array of when each string has exactly two or three the same
   let a = length $ Prelude.filter (>= 1) $ Prelude.map exactTwo x
   let b = length $ Prelude.filter (>= 1) $ Prelude.map exactThree x
+  --find the checksum
   let ans = a * b
   print ans
+  --find the similarities between the two common strings
   let pos = findpos $ L.lines contents
   print pos
 
+--Converts an array of strings into an array of tuples
+--that shows the char and the number of occurances
 frequency :: (Ord a) => [a] -> [(a, Int)]
 frequency xs = toList (fromListWith (+) [(x,1) | x <- xs])
 
+--checks if the char occurs twice
 exactTwo :: (Ord a) => [(a, Int)] -> Int
 exactTwo [] = 0
 exactTwo (x:xs) = val + exactTwo xs
   where val = if snd x == 2 then 1 else 0
 
+--checks if the char occurs three times
 exactThree :: (Ord a) => [(a, Int)] -> Int
 exactThree [] = 0
 exactThree (x:xs) = val + exactThree xs
   where val = if snd x == 3 then 1 else 0
 
-similar :: (Ord a) => [a] -> [a] -> [a]
-similar x y =  [i | (i , j) <- list, i /= j]
+--finds any values that are different
+different :: (Ord a) => [a] -> [a] -> [a]
+different x y =  [i | (i , j) <- list, i /= j]
   where list = zip x y
 
+--Goes through list and finds if the two strings have one char different
+--then gives me the remaining string
 findpos :: [String] -> [Char]
 findpos (x:xs)
   | z /= Nothing = [i | (i,j) <- zip x (xs L.!! (\(Just i) -> i) z), i == j]
   | xs == [] = []
   | otherwise = findpos xs
-  where z = L.findIndex (== 1) $ Prelude.map length $ Prelude.map (similar x) xs
+  where z = L.findIndex (== 1) $ Prelude.map length $ Prelude.map (different x) xs
